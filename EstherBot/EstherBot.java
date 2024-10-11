e962736aa5e6db661a2c499ee196d9c6253f61aa
EstherCho-7 <dkgkqm777@gmail.com>
2024-10-11 16:28:26 +0900

    Advanced

--- EstherBot/EstherBot.java
+++ EstherBot/EstherBot.java
@@ -1,46 +1,49 @@
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 
-// ------------------------------------------------------------------
-// MyFirstBot
-// ------------------------------------------------------------------
-// A sample bot original made for Robocode by Mathew Nelson.
-// Ported to Robocode Tank Royale by Flemming N. Larsen.
-//
-// Probably the first bot you will learn about.
-// Moves in a seesaw motion, and spins the gun around at each end.
-// ------------------------------------------------------------------
 public class EstherBot extends Bot {
+
+    // Constructor that loads the bot configuration file
+    EstherBot() {
+        super(BotInfo.fromFile("EstherBot.json"));
+    }
 
     // The main method starts our bot
     public static void main(String[] args) {
         new EstherBot().start();
     }
 
-    // Constructor, which loads the bot config file
-    EstherBot() {
-        super(BotInfo.fromFile("EstherBot.json"));
-    }
-
-    // Called when a new round is started -> initialize and do some movement
+    // Called when a new round is started
     @Override
     public void run() {
-        // Repeat while the bot is running
+        // Set a random movement pattern
         while (isRunning()) {
-            forward(100);
-            turnGunRight(360);
-            back(100);
+            // Move in a random direction
+            double distance = Math.random() * 100 + 50; // Move between 50 and 150
+            double angle = Math.random() * 360; // Random angle
+            turnRight(angle);
+            forward(distance);
+
+            // Spin the gun and fire at enemies
             turnGunRight(360);
         }
     }
 
-    // We saw another bot -> fire!
+    // Called when another bot is scanned
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        fire(1);
+        // Get the distance to the scanned bot
+        double distance = e.getDistance();
+        
+        // If the enemy is close, evade by turning away and moving back
+        if (distance < 200) {
+            evade(e);
+        } else {
+            fire(1); // Otherwise, fire at the enemy
+        }
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
+    // Called when we are hit by a bullet
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
         // Calculate the bearing to the direction of the bullet
@@ -48,5 +51,18 @@ public class EstherBot extends Bot {
 
         // Turn 90 degrees to the bullet direction based on the bearing
         turnLeft(90 - bearing);
+        forward(50); // Move forward after evading
+    }
+
+    // Method to evade an enemy
+    private void evade(ScannedBotEvent e) {
+        // Get the bearing to the enemy
+        double enemyBearing = e.getBearing();
+
+        // Turn away from the enemy
+        turnLeft(90 + enemyBearing);
+        // Move backward for a short distance
+        back(100);
     }
 }
+
