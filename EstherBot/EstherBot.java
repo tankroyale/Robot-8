dd8cb1b28502cacd5d80e6ef24cb1d07aca76ec5
EstherCho-7 <dkgkqm777@gmail.com>
2024-10-11 18:38:49 +0900

    SpinBot

--- EstherBot/EstherBot.java
+++ EstherBot/EstherBot.java
@@ -1,68 +1,62 @@
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 
+// ------------------------------------------------------------------
+// SpinBot
+// ------------------------------------------------------------------
+// A sample bot original made for Robocode by Mathew Nelson.
+// Ported to Robocode Tank Royale by Flemming N. Larsen.
+//
+// Moves in a circle, firing hard when an enemy is detected.
+// ------------------------------------------------------------------
 public class EstherBot extends Bot {
-
-    // Constructor that loads the bot configuration file
-    EstherBot() {
-        super(BotInfo.fromFile("EstherBot.json"));
-    }
 
     // The main method starts our bot
     public static void main(String[] args) {
         new EstherBot().start();
     }
 
-    // Called when a new round is started
+    // Constructor, which loads the bot config file
+    EstherBot() {
+        super(BotInfo.fromFile("EstherBot.json"));
+    }
+
+    // Called when a new round is started -> initialize and do some movement
     @Override
     public void run() {
-        // Set a random movement pattern
-        while (isRunning()) {
-            // Move in a random direction
-            double distance = Math.random() * 100 + 50; // Move between 50 and 150
-            double angle = Math.random() * 360; // Random angle
-            turnRight(angle);
-            forward(distance);
+        setBodyColor(Color.BLUE);
+        setTurretColor(Color.BLUE);
+        setRadarColor(Color.BLACK);
+        setScanColor(Color.YELLOW);
 
-            // Spin the gun and fire at enemies
-            turnGunRight(360);
+        // Repeat while the bot is running
+        while (isRunning()) {
+            // Tell the game that when we take move, we'll also want to turn right... a lot
+            setTurnLeft(10_000);
+            // Limit our speed to 5
+            setMaxSpeed(5);
+            // Start moving (and turning)
+            forward(10_000);
         }
     }
 
-    // Called when another bot is scanned
+    // We scanned another bot -> fire hard!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        // Get the distance to the scanned bot
-        double distance = e.getDistance();
-        
-        // If the enemy is close, evade by turning away and moving back
-        if (distance < 200) {
-            evade(e);
-        } else {
-            fire(1); // Otherwise, fire at the enemy
+        fire(3);
+    }
+
+    // We hit another bot -> if it's our fault, we'll stop turning and moving,
+    // so we need to turn again to keep spinning.
+    @Override
+    public void onHitBot(HitBotEvent e) {
+        var direction = directionTo(e.getX(), e.getY());
+        var bearing = calcBearing(direction);
+        if (bearing > -10 && bearing < 10) {
+            fire(3);
+        }
+        if (e.isRammed()) {
+            turnLeft(10);
         }
     }
-
-    // Called when we are hit by a bullet
-    @Override
-    public void onHitByBullet(HitByBulletEvent e) {
-        // Calculate the bearing to the direction of the bullet
-        var bearing = calcBearing(e.getBullet().getDirection());
-
-        // Turn 90 degrees to the bullet direction based on the bearing
-        turnLeft(90 - bearing);
-        forward(50); // Move forward after evading
-    }
-
-    // Method to evade an enemy
-    private void evade(ScannedBotEvent e) {
-        // Get the bearing to the enemy
-        double enemyBearing = e.getBearing();
-
-        // Turn away from the enemy
-        turnLeft(90 + enemyBearing);
-        // Move backward for a short distance
-        back(100);
-    }
 }
-
